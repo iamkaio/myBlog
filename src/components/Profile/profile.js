ca328a862ae60d5f9cc75f53837d92e49bbc43bb
kaioalmeidacost <hello@imkaio.com>
2020-07-15 00:29:57 -0300

    Profile component

---
+++ src/components/Profile/profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Kaio Almeida</h1>
+        <h2>UI Developer Designer</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet animi beatae distinctio doloribus,
+            eligendi facere hic inventore minima molestiae odio.</p>
+    </div>
+)
+
+export default Profile
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,20 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Profile from "./Profile/profile";
 
-
-
-const Layout = ({ children }) => {
-  return (
-        <main>{children}</main>
-  )
+const Layout = ({children}) => {
+    return (
+        <>
+            <header>
+              <Profile/>
+            </header>
+            <main>{children}</main>
+        </>
+    )
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 }
 
 export default Layout
