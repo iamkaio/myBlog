fc512306afddc6b634c7f8500b126833cf560dce
kaioalmeidacost <hello@imkaio.com>
2020-07-15 01:58:05 -0300

    add hooks

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -1,12 +1,29 @@
 import React from "react";
+import { useStaticQuery, graphql } from 'gatsby'
 
-const Profile = () => (
-    <div className="Profile-wrapper">
-        <h1>Kaio Almeida</h1>
-        <h2>UI Developer Designer</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet animi beatae distinctio doloribus,
-            eligendi facere hic inventore minima molestiae odio.</p>
-    </div>
-)
+const Profile = () => {
+    const {
+        site: {
+            siteMetadata: {title, position, description},
+        }
+    } = useStaticQuery(graphql`
+        query siteMetadata {
+          site {
+            siteMetadata {
+              description,
+              title,
+              position,
+            }
+          }
+        }
+    `)
+    return(
+        <div className="Profile-wrapper">
+            <h1>{title}</h1>
+            <h2>{position}</h2>
+            <p>{description}</p>
+        </div>
+    )
+}
 
 export default Profile
